3c6fca5b339548c12ce046e5008c304503d3add6
Marcos Gomez <marcos_go2015@hotmail.com>
2020-11-18 07:18:38 -0300

    15. Pruebas faltantes del componente SearchScreen

--- src/tests/components/search/SearchScreen.test.js
+++ src/tests/components/search/SearchScreen.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
+/* import React from 'react';
 import { mount } from 'enzyme';
 import { MemoryRouter, Route } from 'react-router-dom';
 import { SearchScreen } from '../../../components/search/SearchScreen';
-
+ */
 
 describe('Pruebas en <SearchScreen />', () => {
 
@@ -33,7 +33,50 @@ describe('Pruebas en <SearchScreen />', () => {
     })
 
     
-  
+    test('debe de mostrar un error si no se encuentra el Hero', () => {
+        
+        /* const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <Route path="/search" component={ SearchScreen } />
+            </MemoryRouter>
+        );
+
+        expect( wrapper.find('.alert-danger').text().trim() ).toBe(`There is no a hero with batman123`);
+        expect( wrapper ).toMatchSnapshot(); */
+
+    })
+    
+    
+    test('debe de llamar el push del history', () => {
+
+        /* const history = {
+            push: jest.fn()
+        };
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <Route 
+                    path="/search" 
+                    component={ () => <SearchScreen history={ history } /> } 
+                />
+            </MemoryRouter>
+        );
+
+        wrapper.find('input').simulate('change', {
+            target: {
+                name: 'searchText',
+                value: 'batman'
+            }
+        });
+
+        wrapper.find('form').prop('onSubmit')({
+            preventDefault(){}
+        });
+
+        expect( history.push ).toHaveBeenCalledWith(`?q=batman`); */
+
+        
+    })
     
 
     
